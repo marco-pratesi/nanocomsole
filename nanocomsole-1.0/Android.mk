9907ed0d7071a933e8499a55d499d6bd9c89c3fc
Vu Ngoc Trong <vungoctrong@gmail.com>
2017-02-28 16:25:28 +0700

    Update Android.mk

--- nanocomsole-1.0/Android.mk
+++ nanocomsole-1.0/Android.mk
@@ -2,11 +2,11 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(call all-subdir-makefiles)
 include $(CLEAR_VARS)
-
+FILE_LIST := $(wildcard $(LOCAL_PATH)/*.c)
 #######################################################
 LOCAL_MODULE := nanoconsole
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
-LOCAL_SRC_FILES := menu.c mux.c nanocomsole.c
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 LOCAL_LDLIBS := -llog
 LOCAL_CFLAGS += -fPIE
 LOCAL_LDFLAGS += -fPIE -pie
